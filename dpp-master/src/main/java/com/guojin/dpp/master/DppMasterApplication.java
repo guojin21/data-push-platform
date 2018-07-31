4439674bbc4111ea7ae99561b808f181e431aa0e
guojin <guojin@channelsoft.com>
2018-07-31 16:59:36 +0800

    新增父pom依赖，并调整各父子模块pom间的继承与依赖关系；新增dpp_master一键启动；测试swagger定义的接口[http://localhost:8080/hello]可用

--- dpp-master/src/main/java/com/guojin/dpp/master/DppMasterApplication.java
+++ dpp-master/src/main/java/com/guojin/dpp/master/DppMasterApplication.java
@@ -3,7 +3,11 @@ package com.guojin.dpp.master;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.guojin.dpp.master",
+        "com.guojin.dpp.web.*",
+}
+)
 public class DppMasterApplication {
 
     public static void main(String[] args) {
---
+++ dpp-master/src/main/java/com/guojin/dpp/master/SwaggerConfig.java
@@ -0,0 +1,60 @@
+package com.guojin.dpp.master;
+
+import com.google.common.base.Predicate;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        Predicate<RequestHandler> predicate = new Predicate<RequestHandler>() {
+          @Override
+          public boolean apply(RequestHandler input) {
+                Class<?> declaringClass = input.declaringClass();
+                if (declaringClass == BasicErrorController.class) {
+                    return false;
+                }
+
+                if (declaringClass.isAnnotationPresent(RestController.class)) {
+                    return true;
+                }
+
+              if (input.isAnnotatedWith(ResponseBody.class)) {
+                  return true;
+              }
+
+              return false;
+          }
+        };
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(predicate)
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("数据推送平台接口")
+                .description("data-push-platform")
+                .contact(new Contact("guojin", "", ""))
+                .version("1.0.0")
+                .build();
+    }
+
+}
